63cf990199ea5f974eb3bd6ed2a9826b6b508419
sivaganesh k <gansiva5@gmail.com>
2024-10-04 14:25:53 +0530

    changes

--- index.js
+++ index.js
@@ -2,20 +2,17 @@ function change(col){
     if(col==='blue'){
         document.body.style.backgroundColor="blue";
         document.getElementById("b").style.backgroundColor="white"
-        // document.getElementById("r").style.backgroundColor="white"
-        // document.getElementById("g").style.backgroundColor="white"
     }
     else if(col==='red'){
         document.body.style.backgroundColor="red";
         document.getElementById("r").style.backgroundColor="white"
-        // document.getElementById("b").style.backgroundColor="white"
-        // document.getElementById("g").style.backgroundColor="white"
+        document.getElementById("b").style.backgroundColor="blue"
     }
     else if(col==='green'){
         document.body.style.backgroundColor="green";
         document.getElementById("g").style.backgroundColor="white"
-        // document.getElementById("b").style.backgroundColor="white"
-        // document.getElementById("r").style.backgroundColor="white"
+        document.getElementById("b").style.backgroundColor="blue"
+        document.getElementById("r").style.backgroundColor="red"
 
     }
     else{
